7e322a62598ecefbe4d342dcd7e45c2f01633109
汪吉 <wj181037@gongdao.com>
2022-01-25 18:42:37 +0800

    add download url

--- start-consumer.sh
+++ start-consumer.sh
@@ -5,3 +5,5 @@ mvn clean package -Dmaven.test.skip=true && \
 java -javaagent:/Users/wangji/dev/skywalking-agent/skywalking-agent.jar -Dskywalking.agent.service_name=consumer \
 -jar $dir/dubbo-consumer/target/dubbo-consumer.jar
 
+# 下载地址 skywalking-agent
+#https://www.apache.org/dyn/closer.cgi/skywalking/java-agent/8.8.0/apache-skywalking-java-agent-8.8.0.tgz
--- start-provider.sh
+++ start-provider.sh
@@ -5,3 +5,5 @@ mvn clean package -Dmaven.test.skip=true && \
 java -javaagent:/Users/wangji/dev/skywalking-agent/skywalking-agent.jar -Dskywalking.agent.service_name=provider \
 -jar $dir/dubbo-provider/target/dubbo-provider.jar
 
+# 下载地址 skywalking-agent
+#https://www.apache.org/dyn/closer.cgi/skywalking/java-agent/8.8.0/apache-skywalking-java-agent-8.8.0.tgz
